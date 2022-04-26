3b269b42a59125389363688430a7fbb91e98d7e9
m-degalicia <35519542+m-degalicia@users.noreply.github.com>
2022-04-27 00:02:02 +0800

    integrate fetch to table, add pagination, filter

--- src/pages/table/index.tsx
+++ src/pages/table/index.tsx
@@ -1,10 +1,13 @@
-import React from "react";
-import { useAppSelector } from "app/hooks";
-import { selectCharacters } from "redux/genshinCharacters/genshinCharactersSlice";
-import { DataGrid, GridColDef } from "@mui/x-data-grid";
+import React, { useEffect, useState } from "react";
+import { useAppSelector, useAppDispatch } from "app/hooks";
+import {
+  selectCharacters,
+  fetchCharactersList,
+} from "redux/genshinCharacters/genshinCharactersSlice";
+import { DataGrid, GridColDef, GridToolbar } from "@mui/x-data-grid";
 
 const columns: GridColDef[] = [
-  { field: "name", headerName: "Name" },
+  { field: "name", headerName: "Name", width: 150 },
   { field: "vision", headerName: "Vision" },
   { field: "weapon", headerName: "Weapon" },
   { field: "nation", headerName: "Nation" },
@@ -13,11 +16,32 @@ const columns: GridColDef[] = [
 ];
 
 const TablePage = () => {
+  const [pageSize, setPageSize] = useState<number>(5);
   const characterList = useAppSelector(selectCharacters);
+  const characterListStatus = useAppSelector(
+    (state) => state.genshinCharacters.status
+  );
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (characterListStatus === "idle") {
+      dispatch(fetchCharactersList());
+    }
+  }, [characterListStatus, dispatch]);
+
   return (
     <div style={{ display: "flex", height: "100%", width: "100%" }}>
       <div style={{ flexGrow: 1 }}>
-        <DataGrid autoHeight rows={characterList} columns={columns} />
+        <DataGrid
+          autoHeight
+          rows={characterList}
+          columns={columns}
+          pageSize={pageSize}
+          onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+          rowsPerPageOptions={[5, 10, 20]}
+          pagination
+          components={{ Toolbar: GridToolbar }}
+        />
       </div>
     </div>
   );
