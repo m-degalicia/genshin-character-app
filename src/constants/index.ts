0bfcbade81e8198049f5554a86ba7b1444670748
m-degalicia <35519542+m-degalicia@users.noreply.github.com>
2022-04-26 23:02:17 +0800

    update constants capitalization

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,6 @@
 export const SERVER_URL = 'https://api.genshin.dev/';
 
-export const ElementsList = ['anemo', 'cryo' ,'dendro' ,'electro' ,'geo' ,'hydro' , 'pyro'] as const;
-export const WeaponsList = ['sword', 'claymore', 'catalyst', 'bow', 'polearm'] as const;
+export const ElementsList = ['Anemo', 'Cryo' ,'Dendro' ,'Electro' ,'Geo' ,'Hydro' , 'Pyro'] as const;
+export const WeaponsList = ['Sword', 'Claymore', 'Catalyst', 'Bow', 'Polearm'] as const;
 export const NationsList = ['Mondstadt', 'Liyue', 'Inazuma', 'Fontaine', 'Snezshnaya', 'Sumeru', 'Natlan'] as const;
 export const RarityList = [4, 5] as const;
