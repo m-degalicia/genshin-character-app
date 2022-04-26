38f8d76b305131f6c3da4e0652d041a39bd04644
m-degalicia <35519542+m-degalicia@users.noreply.github.com>
2022-04-26 22:11:55 +0800

    add constants

---
+++ src/constants/index.ts
@@ -0,0 +1,6 @@
+export const SERVER_URL = 'https://api.genshin.dev/';
+
+export const ElementsList = ['anemo', 'cryo' ,'dendro' ,'electro' ,'geo' ,'hydro' , 'pyro'] as const;
+export const WeaponsList = ['sword', 'claymore', 'catalyst', 'bow', 'polearm'] as const;
+export const NationsList = ['Mondstadt', 'Liyue', 'Inazuma', 'Fontaine', 'Snezshnaya', 'Sumeru', 'Natlan'] as const;
+export const RarityList = [4, 5] as const;
