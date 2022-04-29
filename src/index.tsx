3976aef34eed921d30eee10a1b523732504ddee4
m-degalicia <35519542+m-degalicia@users.noreply.github.com>
2022-04-29 12:59:24 +0800

    move App to app folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
-import { store } from "./app/store";
-import App from "./App";
+
+import App from "app";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
 
@@ -12,11 +10,7 @@ const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter basename={`/${process.env.PUBLIC_URL}`}>
-        <App />
-      </BrowserRouter>
-    </Provider>
+    <App />
   </React.StrictMode>
 );
 
